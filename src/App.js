08cf54b58d3a71b36a55028e0bd8b9661e569a02
Camil Grabowski <camil.grabowski@uconn.edu>
2021-10-15 15:02:41 -0400

    finished contact

--- src/App.js
+++ src/App.js
@@ -1,37 +1,48 @@
-import React, { useState } from 'react';
-import Nav from './components/Nav';
-import About from './components/About';
-import Gallery from './components/Gallery';
+import React, { useState } from "react";
+import Nav from "./components/Nav";
+import About from "./components/About";
+import Gallery from "./components/Gallery";
+import ContactForm from "./components/Contact";
 
 function App() {
   const [categories] = useState([
     {
-      name: 'commercial',
-      description: 'Photos of grocery stores, food trucks, and other commercial projects',
+      name: "commercial",
+      description:
+        "Photos of grocery stores, food trucks, and other commercial projects",
     },
-    { name: 'portraits', description: 'Portraits of people in my life' },
-    { name: 'food', description: 'Delicious delicacies' },
-    { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' },
+    { name: "portraits", description: "Portraits of people in my life" },
+    { name: "food", description: "Delicious delicacies" },
+    {
+      name: "landscape",
+      description: "Fields, farmhouses, waterfalls, and the beauty of nature",
+    },
   ]);
 
-  const [currentCategory, setCurrentCategory] = useState(categories[0]);    //need usestate explained
+  const [currentCategory, setCurrentCategory] = useState(categories[0]); //need usestate explained
+
+  const [contactSelected, setContactSelected] = useState(false);
 
   return (
     <div>
       <Nav
-        categories={categories}                                               // need this explained
+        categories={categories}               // Passing the getter and setter functions into the Nav component will allow this component to modify the state in the App component, which will conditionally render based on the user's selection
         setCurrentCategory={setCurrentCategory}
         currentCategory={currentCategory}
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected}
       ></Nav>
       <main>
-        <div>
-        <Gallery currentCategory={currentCategory}></Gallery>
-          <About></About>
-        </div>
+        {!contactSelected ? (                   //if the contactSelected is false, the Gallery and About components should be rendered, but if contactedSelected is true, the ContactForm component should be rendered.
+          <>
+            <Gallery currentCategory={currentCategory}></Gallery>
+            <About></About>
+          </>
+        ) : (
+          <ContactForm></ContactForm>
+        )}
       </main>
     </div>
   );
 }
 export default App;
-
-
